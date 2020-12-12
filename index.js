9537138ec452643ec1674031bccad820af0d3edd
JosephMartinez3005 <52721764+JosephMartinez3005@users.noreply.github.com>
2020-12-11 19:31:26 -0800

    Finsihed

--- index.js
+++ index.js
@@ -74,15 +74,24 @@ class Airplane {
       - Give cars the ability to get fueled with a `.fill(gallons)` method. Add the gallons to `tank`.
       - Give cars ability to `.drive(distance)`. The distance driven:
           + Should cause the `odometer` to go up.
-          + Should cause the the `tank` to go down taking `milesPerGallon` into account.
+          + Should cause the `tank` to go down taking `milesPerGallon` into account.
       - A car which runs out of `fuel` while driving can't drive any more distance:
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
-  
- class Car {
-    
-  }
-  
+  class Car {
+  constructor(model, milesPerGallon){
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
+}
+fill(gallons){
+this.tank = this.tank + gallons;
+}
+drive(distance){
+ 
+  } 
+}
   /*
     TASK 3
       - Write a Lambdasian class.
@@ -96,9 +105,16 @@ class Airplane {
           + {name} and {location} of course come from the instance's own properties.
   */
  class Lambdasian {
-    
+  constructor(att){
+    this.name = att.name;
+    this.age = att.age;
+    this.location = att.location;
   }
-  
+  speak(){
+    return`Hello my name is ${this.name}, I am from ${this.location}` 
+  }
+}
+
   /*
     TASK 4
       - Write an Instructor class extending Lambdasian.
@@ -113,9 +129,23 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
+ class Instructor extends Lambdasian{
+  constructor(att){
+    super(att)
+    this.specialty = att.specialty;
+    this.favLanguage = att.favLanguage;
+    this.catchPhrase = att.catchPhrase;
+  }
+    demo(subject){
+      return `Today we are learning about ${subject}`
+    }
+    grade(student,subject){
+      return `${student.name} receives a perfect score on ${subject}`
+  }
 
+  
  }
+ 
   /*
     TASK 5
       - Write a Student class extending Lambdasian.
@@ -131,8 +161,22 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
-     
+ class Student extends Lambdasian{
+     constructor(att){
+       super(att);
+       this.previousBackground = att.previousBackground;
+       this.className = att.className;
+       this.favSubjects = att.favSubjects;
+     }
+     listSubjects(){
+      this.favSubjects.forEach((item) => {return `Loving ${this.favSubjects.item}!`})
+     }
+     PRAssignment(subject){
+       return `${this.name} has submitted a PR for ${subject}`
+     }
+     sprintChallenge(subject){
+      return `${this.name} has begun sprint challenge on ${subject}`
+     }
  }
   
   /*
@@ -148,8 +192,18 @@ class Airplane {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
-     
+ class ProjectManager extends Instructor {
+     constructor(att){
+       super(att);
+       this.gradClassName = att.gradClassName;
+       this.favInstructor = att.favInstructor
+     }
+     standUp(channel){
+       return `${this.name} announces to ${channel}, @channel standy times!`
+     }
+     debugsCode(student, subject){
+       return ` ${this.name} debugs ${student.name}'s code on ${this.subject}`
+     }
  }
   /*
     STRETCH PROBLEM (no tests!)
